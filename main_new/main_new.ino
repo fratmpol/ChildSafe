71dcbe1838eabd54965e2df0ea870fc0bccc956b
fratmpol <francesco.polcri.spam@gmail.com>
2023-11-17 13:53:12 +0100

    new main created (WIP)

--- main_new/main_new.ino
+++ main_new/main_new.ino
@@ -1,75 +1,118 @@
 #include <Arduino.h>
 
-// PIN DEFINITIONS
+// PIN DEFINITIONS //
 #define VCCSensPin 1
-#define vibPin 3
-#define accPin 4
-#define RFIDPin 5
-#define PIRPin 10
-#define radPin 11
-#define CO2Pin 12
-#define tempPin 13
-#define ultraPin 14
-#define N 8
+// I/O sensors pins
+#define motorVibPin 2
+#define accPin 3
+#define ultraSeatPin 4
+// Detection sensor pins
+#define PIRPin 5
+#define radPin 6
+#define CO2Pin 7
+// control sensors pins
+#define tempPin 8
+#define ultraWindPin 9
+#define radarVibPin 10
+// number of sensors for each task
+#define NIO 3
+#define NDET 3
+#define NCON 3
+#define NSENS NIO+NDET+NCON
 ////////////////////////////////// led pins for logic configuration
 #define OFFPin 7
 #define checkPin 8
 #define ONPin 9
 
-int pins[N] = {vibPin,accPin,RFIDPin,ultraPin,tempPin,PIRPin,radPin,CO2Pin}; /// think about using the arrays
+// PIN ARRAYS //
+int acc[NIO] = {motorVibPin,accPin,ultraSeatPin};
+int det[NDET] = {PIRPin,radPin,CO2Pin};
+int con[NCON] = {tempPin,ultraWindPin,radarVibPin};
+int sens[NSENS] = {motorVibPin,accPin,ultraSeatPin,PIRPin,radPin,CO2Pin,tempPin,ultraWindPin,radarVibPin};
 
-// WEUGHTS DEFINITION
+// STATE DEFINITION //
+#define OFF 0
+#define ON 1
+#define CHECK 2
+#define CHILD_DETECTED 3
+
+// STATE ARRAY //
+int state_array[4] = {OFF,ON,CHECK,CHILD_DETECTED}; //may be not used
+
+// SYSTEM MAIN VARIABLE //
+int state = OFF;
+
+// WEiGHTS DEFINITION //
 #define LOWERED_WINDOW 0
-#define CLOSED_WINDOW 1
+#define CLOSED_WINDOW 1 
+#define T_CONST 1 //to be tuned
 
-// GENERAL VARIABLES DEFINITION //
-// delay variables for state sensors
+// DELAYS //
+// delay for CHECK state
 #define DELAY_VIB 5000 // slower if the car has start and stop
 #define DELAY_ACC 5000 // accelerometer delay must be slower than the others two
 #define DELAY_RFID 5000
-// delay variables for the time treshold
+// delay for the time treshold
 #define SHUTDOWN_DELAY 100 // (TO BE TUNED TO BE 40 MINUTES)
 #define SHUTDOWN_TIME 4400/SHUTDOWN_DELAY // NB: 0.1millisecond scale (TO BE TUNED TO BE 40 MINUTES)
+//delay for before and after measurement
+#define OLD_NEW_DELAY 10
 
+// SHUTDOWN TIMER VARIABLE
+int shut_t = 0;
+
+// CONTROL SENSOR TIMER
+int control_t = 0;
+
+// TRESHOLD FOR DETECTION //
 #define TRESHOLD 1
 
 
-
-// STATE DEFINITIONS
-#define OFF 0
-#define ON 1
-#define CHILD_DETECTED 2
-#define CHECK 3  /////[REMOVE IN FINAL CODE]///// variable for the check state of the system before the powering on
-// system state variable
-int state = OFF;
-// sensor alimentation state
+// SENSOR ALIMENTATION STATE
 int VCCSens = 0;
 
 // CPD VARIABLES
-// shutdown timer variable
-int shut_t = 0;
 // measurments results of the detection sensors
 int det_res[3] = {0,0,0};
 // weights of the sensors
 int det_weights[3] = {0,0,0};
-// control sensors timer
-int control_t = 0;
 // sum of the measurement
 int sum = 0;
+
+//check variable for the activation of the system (if possible replace with a pointer)
+int check[NIO]; 
+
+
+
+
+
 
 
 
 void setup() {
+
   // opening the serial monitoring
   Serial.begin(115200);
   Serial.println("-------STARTING-------");
-  // sensors initialization
-  for(int i=0; i<8; i++){
-    pinMode(pins[i],INPUT);
+
+  // SENSORS INITIALIZATION
+  for(int i=0; i<NSENS; i++){
+    pinMode(sens[i],INPUT);
     Serial.print("INPUT: "); Serial.println(i);
   }
-  pinMode(VCCSensPin,OUTPUT);
+
+  //INITIALIZATION OF ARRAYS
+  for(int i = 0; i<NIO; i++){
+    check[i] = 0;
+    Serial.print("check("); Serial.print(i); Serial.print(") : "); Serial.println(check[i]);
+  }
+
+  // ALIMENTATION INITIALAZATION
+  pinMode(VCCSensPin,OUTPUT);  
+
 }
+
+
 
 
 
@@ -122,11 +165,37 @@ void loop() {
     control_t--;
   }
 
+  if(state == OFF){
+
+    // controls if something has turned off
+    check_function();
+    // if something has turned off 
+
+
+  }else if(state == CHECK){
+
+
+
+  }else if(state == ON){
+
+
+
+  }else if(state == CHILD_DETECTED){
+
+
+
+  }
+
   delay(SHUTDOWN_DELAY); //main delay of the system needed to operate the time resolution of some cicles (may be removed in future)
   delay(SHUTDOWN_DELAY*10); /////[REMOVE IN FINAL CODE]///// delay introduced for logic monitoring (debugging) [REMOVE IN FINAL CODE]
   led_state(state);  /////[REMOVE IN FINAL CODE]///// led state function to illuminate the leds
  
 }
+
+
+
+
+
 
 
 
@@ -147,6 +216,37 @@ void system_switch(int sswitch){
   }
 }
 
+///////////////////////////////////////////////////////////////////
+void check_function(void){
+
+    int old[NIO];
+    int current[NIO];
+
+    for(int i=0; i<NIO; i++){
+        old[i] = 0;
+        current[i] = 0;
+    }
+
+    old[0] = is_vibrating();
+    old[1] = is_accelerating();
+    old[2] = is_moving();
+
+    delay(OLD_NEW_DELAY);
+
+    current[0] = is_vibrating();
+    current[1] = is_accelerating();
+    current[2] = is_moving();
+
+    //for each sensor if the new measured value it's different from the previous measured return 1 else return 0
+    for(int i=0; i<NIO; i++){
+        if(old != current){
+            check[i] = 1;
+        }else{
+            check[i] = 0;
+        }
+    }
+    
+}
 
 // check for the vibration of the motor
 int is_vibrating(void){
@@ -163,7 +263,7 @@ int is_vibrating(void){
 
 
 // check for the RFID inside the car
-int RFID_check(void){
+int is_moving(void){
   if (digitalRead(RFIDPin)==LOW){
     Serial.println("REFID -> OFF");
     return 0;
@@ -322,4 +422,3 @@ void led_state(int ledPin){
     Serial.println("Error in state decision");
   }
 }
-
